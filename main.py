6ca386cf6845ee33eda57fdcf55618b29465edec
Vera Spiridonova <ver-spiridonova@yandex.ru>
2024-12-26 13:41:02 +0300

    Added task2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import types
 from task_1 import FlatIterator
 
 
@@ -19,5 +20,32 @@ def test_1():
     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
 
+def flat_generator(list_of_lists):
+    for lst in list_of_lists:
+        for item in lst:
+            yield item
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
 if __name__ == '__main__':
     test_1()
+    test_2()
